1bea27aa023b3a485c2ffadf4d9928d3d2a80e1a
Evelyn Bart <evelyn.m.bart@gmail.com>
2025-01-28 22:08:08 +0000

    Add category selection UI with styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import { BrowserRouter as Router, Routes } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <Router>
-      <div className="app">
-        <Routes></Routes>
-      </div>
+      <main className="app">
+        <h1>Th!nk</h1>
+        <h2>Sharpen your skills, one question at a time</h2>
+        <section className="pick-categories">
+          <h3>Pick a category</h3>
+          <div className="categories">
+            <button id="history-geography">History & Geography</button>
+            <button id="science-nature">Science & Nature</button>
+            <button id="literature-art">Art & Literature</button>
+            <button id="pop-culture">Pop Culture</button>
+            <button id="sports-leisure">Sports & Leisure</button>
+          </div>
+        </section>
+      </main>
     </Router>
   );
 }
